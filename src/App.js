801a2ca5cbf6825119f035e21adc599abcebcb66
Julita <julia.franchi7@gmail.com>
2023-11-14 16:41:12 -0300

    squares

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Board from './components/Board/Board';
 
 const App = () => {
 	const [turn, setTurn] = useState('x'); // siempre comenzaran las X
-	const [squares, setSquares] = useState([1, 2, 3, 4, 5, 6, 7, 8, null]);
+	const [squares, setSquares] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, null]);
 	//representa a los cuadrados con un array
 	//de 9 espacios que empiezan en null - .fill = cambia los valores de un array por un valor estatico
 	const [score, setScore] = useState({
